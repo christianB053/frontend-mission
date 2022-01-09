59e7240680c01de6f607b981feb9723b837effd5
Hyunbin Kim <binikim66@gmail.com>
2022-01-09 19:51:30 +0900

    1st week input, button code

--- mission/src/views/Home.vue
+++ mission/src/views/Home.vue
@@ -3,6 +3,12 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <div id="two-way-binding">
+  <input v-model="message" />
+  <button v-on:click="changeMessage">제출</button>
+  <button v-on:click="alertButton">알림</button>
+  <p>{{ message }}</p>
+  </div>
 </template>
 
 <script>
@@ -14,5 +20,30 @@ export default {
   components: {
     HelloWorld,
   },
+  data() {
+    return {
+      message: '',
+      counter: 0,
+    };
+  },
+  methods: {
+    changeMessage() {
+      this.message = this.message
+        .split('')
+        .splice(1)
+        .join('');
+    },
+    alertMessage() {
+      alert(this.message);
+    },
+    counterMessage() {
+      alert(this.counter += 1);
+    },
+    alertButton() {
+      this.alertMessage();
+      this.counterMessage();
+    },
+  },
 };
+
 </script>
